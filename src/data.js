d71ff09156d9eeb71b68d9957196310760a74875
Margot <maria16ruiz81@gmail.com>
2020-02-28 11:28:25 -0600

    prueba sobre html

--- src/data.js
+++ src/data.js
@@ -1,12 +1,17 @@
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
+
+import data from './data/pokemon/pokemon.js';
+
+export const prueba = pokemon.filter (num => pokemon.num => 5 ); 
+console.log(prueba);
+
 
 export const example = () => {
   return 'example';
