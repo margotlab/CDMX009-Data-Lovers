00c8f7795db99a0876993aeb8238f687b52f9b7f
Margot <maria16ruiz81@gmail.com>
2020-03-02 08:42:16 -0600

    prueba flexbox

--- src/data.js
+++ src/data.js
@@ -4,14 +4,9 @@
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
+// import data from './data/pokemon/pokemon.js';
 
 // esta es una función de ejemplo
-
-import data from './data/pokemon/pokemon.js';
-
-export const prueba = pokemon.filter (num => pokemon.num => 5 ); 
-console.log(prueba);
-
 
 export const example = () => {
   return 'example';
