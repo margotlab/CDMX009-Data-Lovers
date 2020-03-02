4494c7aeb15de3f021af4b31f717eec047ddfae9
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-03-01 21:43:15 -0600

    Trabajando en main.js

--- src/data.js
+++ src/data.js
@@ -1,13 +1,30 @@
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
 // import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
 // import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
-
 export const example = () => {
-  return 'example';
+    return 'example';
 };
+
+export const pokemones = {
+
+    filterByName(namePoke) {
+        return data.pokemon.filter(pokemon => {
+            return pokemon.name.toLowerCase().includes(namePoke.toLowerCase());
+        });
+    },
+    filterByNumber() {
+
+    },
+    filterByType() {
+
+    },
+    findAll() {
+        return data.pokemon;
+    },
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,50 @@
 import { example } from './data.js';
+import { pokemones } from './data.js';
 
-console.log(example);
+function filterByName(name) {
+    let home = document.getElementById("derecha");
+    let arrayPoke = pokemones.filterByName(name);
 
+    arrayPoke.forEach(pokemon => {
+        let column = generarTarjeta(pokemon);
+        home.appendChild(column);
+    })
+}
+
+function findAll() {
+    let home = document.getElementById("derecha");
+    let arrayPoke = pokemones.findAll();
+
+    arrayPoke.forEach(pokemon => {
+        let column = generarTarjeta(pokemon);
+        home.appendChild(column);
+    })
+}
+
+function generarTarjeta(pokemon) {
+    let column = document.createElement("div");
+    column.setAttribute("class", "col-3 mt-2");
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    let image = document.createElement("img");
+    image.setAttribute("class", "card-img-top");
+    image.setAttribute("alt", pokemon.name);
+    image.setAttribute("src", pokemon.img);
+
+    let cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+
+    let cardText = document.createElement("p");
+    cardText.textContent = `${pokemon.name} ${pokemon.num}`;
+
+    cardBody.appendChild(cardText);
+    card.appendChild(image);
+    card.appendChild(cardBody);
+    column.appendChild(card);
+
+    return column;
+}
+
+findAll();
